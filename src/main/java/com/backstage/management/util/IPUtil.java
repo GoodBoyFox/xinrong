2300604d800546dcf3aff24a1012d775a5fb3345
GoodBoyFox <48684840+GoodBoyFox@users.noreply.github.com>
2021-03-09 16:44:43 +0800

    Merge branch 'main' of F:\github\xinrong with conflicts.

--- src/main/java/com/backstage/management/util/IPUtil.java
+++ src/main/java/com/backstage/management/util/IPUtil.java
@@ -132,6 +132,7 @@ public class IPUtil {
             return localip;
         }
     }
+
     /**
      * @Description：获取客户端外网ip
      * @Author：zrt
@@ -181,6 +182,29 @@ public class IPUtil {
         }
     }
 
+
+    public static String getHostIp(){
+        try{
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()){
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()){
+                    InetAddress ip = (InetAddress) addresses.nextElement();
+                    if (ip != null
+                            && ip instanceof Inet4Address
+                            && !ip.isLoopbackAddress() //loopback地址即本机地址，IPv4的loopback范围是127.0.0.0 ~ 127.255.255.255
+                            && ip.getHostAddress().indexOf(":")==-1){
+                        System.out.println("本机的IP = " + ip.getHostAddress());
+                        return ip.getHostAddress();
+                    }
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
